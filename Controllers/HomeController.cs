54d48d76e72942bf43f449d3f8c752f597950851
Maryana Antonyuk and Marguerite Kennedy <marguerite@>
2019-05-06 13:14:38 -0700

    add initial setup & SQL connection

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using World.Models;
+
+namespace World.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View(Item.GetAll());
+      }
+
+    }
+}
